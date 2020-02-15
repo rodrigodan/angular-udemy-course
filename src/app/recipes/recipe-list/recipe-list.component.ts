2896fb5f1b4a71756c7891d04385fcead67d2e7e
rodrigogoncalves123 <rodrigodaniel@cc.ci.ufpb.br>
2020-02-15 14:59:01 -0300

    Adding content to the recipe components and outputing a list of recipes with the directive ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,13 @@ import { Recipe } from '../recipes.model';
 export class RecipeListComponent implements OnInit {
 
   recipes:Recipe[] = [
-    new Recipe('A test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('A test Recipe', 
+    'This is simply a test', 
+    'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A test Recipe', 
+    'This is simply a test', 
+    'https://familynutrition.co.uk/wp-content/uploads/2014/06/Recipe-icon.png')
+
   ];
 
   constructor() { }
